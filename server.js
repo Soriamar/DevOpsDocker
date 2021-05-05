1d1a50ea756946f072761841cd477cfdd991ae35
soriamar <soriamar@students.zhaw.ch>
2021-05-05 22:35:26 +0200

    new server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 //App
 const app = express();
 app.get('/', (req,res) => {
-    res.send("Hello DevOps Course :)");
+    res.send("Hello Again DevOps Course by Jenkins-Docker-Build-Image");
 });
 
 app.listen(PORT, HOST);
